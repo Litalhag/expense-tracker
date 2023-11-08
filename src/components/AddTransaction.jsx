422e03a00af8945c343b37eca2a1c343f1cd18b4
Litalhag <litalhag@gmail.com>
2023-11-08 20:49:51 +0200

    context, reducer, addTransaction, deleteTransaction

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const AddTransaction = () => {
+  const { addTransaction } = useContext(GlobalContext)
+
   const [text, setText] = useState('')
   const [amount, setAmount] = useState(0)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    }
+
+    addTransaction(newTransaction)
+  }
+
   return (
     <div>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
